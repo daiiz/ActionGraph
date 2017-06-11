687486778f5b6e4b5a55975807192b9a2ce65338
daiiz <da.iz.mg@gmail.com>
2017-06-11 19:06:09 +0900

    利用シーンから考える

--- sample/todoApp/index.js
+++ sample/todoApp/index.js
@@ -1,20 +1,68 @@
 var af = new ActionFlow();
 var ag = af.global();
 
-class PrintOp extends Op {
-  constructor () {
-    super();
-    console.info(888);
-  }
 
-  def (ops) {
+class jQueryAjax extends Op {
+  constructor (name, ops) {super(name, ops);}
+
+  def (successOp=null, failOp=null) {
+    var constOp = this.refOps()[0];
+    var dict = constOp.getAction();
+    $.ajax({
+      url: dict.url,
+      type: 'POST',
+      dataType: 'json',
+      data: dict.data
+    }).success(data => {
+      var c = new Const({data: data});
+      if (successOp) {
+        var op = new successOp(null, [c]);
+        op.run();
+      }
+    }).fail(data => {
+      var c = new Const({data: data});
+      if (failOp) {
+        var op = new failOp(null, [c]);
+        op.run();
+      }
+    });
   }
 }
 
-af.init = {
-};
+class PrintOp extends Op {
+  constructor (name, ops) {super(name, ops);}
 
-ag.print = new PrintOp();
+  def () {
 
-console.info(af);
+    // 参照元のOpのActionを見る
+    var t = 0;
+    var refOps = this.refOps();
+    for (var i = 0; i < refOps.length; i++) {
+      var a = refOps[i].getAction();
+      if (a) t += a.num;
+      console.log('>>', a);
+    }
+
+    t += 1;
+
+    // 非同期処理の書き方
+    this.async(
+      new jQueryAjax('MyAjaxOp', [
+        new Const({
+          url: 'http://daiiz-apps.appspot.com/sb/p/a',
+          method: 'POST',
+          data: {'a': 'A'}
+        })
+      ]), Log, Log);
+    this.storeAction({'num': t});
+  }
+}
+
+var printOp = new PrintOp('SubPrintOp');
+ag.print = new PrintOp('MyPrintOp', [printOp, printOp]);
+
+console.info(af, ag);
 console.log(ag.print);
+
+//ag.print.run();
+console.info('!', ag.print.getAction());
--- src/actionflow.js
+++ src/actionflow.js
@@ -1,3 +1,6 @@
+var AF_OP_NAME = 'Op';
+var AF_CONST_OP_NAME = 'ConstOp';
+
 class ActionFlow {
   constructor () {
     this.__version__ = '0.0.1';
@@ -14,19 +17,89 @@ class ActionFlow {
 }
 
 class Op {
-  constructor () {
+  constructor (name='Op', ops=[]) {
+    this.name = name;
+    this.argOps = ops;
+    // 定義された内容を実行した結果が格納される
+    this.action = null;
+    this.__id__ = `Op_${Math.floor(Math.random() * 10000000000)}`;
+  }
+
+  identifier () {
+    return (this.name === AF_OP_NAME || this.name === AF_CONST_OP_NAME) ? this.__id__ : this.name;
+  }
+
+  run (recursive=false) {
     this.def();
   }
 
-  run () {}
+  def (successOp, failOp) {}
 
-  def (ops=[]) {
-    console.log(ops.length);
+  refOps () {
+    return this.argOps;
+  }
+
+  refOpActions () {
+    var actions = {};
+    var refOps = this.refOps();
+    for (var i = 0; i < refOps.length; i++) {
+      var op = refOps[i];
+      var a = op.getAction();
+      var name = op.identifier();
+      actions[name] = a;
+    }
+    return actions;
+  }
+
+  storeAction (action={}) {
+    this.action = new Action(action);
+  }
+
+  getAction () {
+    if (this.action) return this.action.__a__;
+    return this.action;
+  }
+
+  // func: 処理内容
+  // op: 処理完了後の遷移先Op
+  async (op, callbackSuccessOp, callbackFailOp) {
+    if (!op) return;
+    console.info(op);
+    op.def(callbackSuccessOp, callbackFailOp);
+  }
+
+  /* 解析用 */
+  analysis () {
+    this.def();
   }
 }
 
 class Action {
-  constructor (val) {
-    this.value = val;
+  constructor (val={}) {
+    this.__a__ = val;
+  }
+}
+
+// builtin-op
+class Const extends Op {
+  constructor (dict, name) {
+    if (!name) name = 'ConstOp';
+    super(name, []);
+    this.def(dict);
+  }
+
+  def (constDict) {
+    this.storeAction(constDict);
+  }
+}
+
+class Log extends Op {
+  constructor (name, ops) {
+    if (!name) name = 'LoggerOp';
+    super(name, ops);
+  }
+
+  def () {
+    console.dir(this.refOpActions());
   }
 }
